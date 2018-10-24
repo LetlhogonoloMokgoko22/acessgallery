338692bf2939f74d56b97f9d06782812535b3c7d
LetlhogonoloMokgoko <47385111@mylife.unisa.ac.za>
2018-10-24 17:18:05 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Camera } from '@ionic-native/camera'
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  base64Image:any;
+
+  constructor(public navCtrl: NavController,public camera:Camera) {
 
   }
+  accessGallery(){
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM,
+      destinationType: this.camera.DestinationType.DATA_URL
+     }).then((imageData) => {
+       this.base64Image = 'data:image/jpeg;base64,'+imageData;
+      }, (err) => {
+       console.log(err);
+     });
+   }
+ }
 
-}
